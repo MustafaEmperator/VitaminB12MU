cabb3a09576f2b590092ef223819e711f607ccbb
MustafaEmperator <mustafab565656@gmail.com>
2025-06-17 08:43:34 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Mustafa, Armando, Issam, Saly</h1>
+      <h2>Vitamin B12 - Infos und Selbsttest</h2>
+      <p>Hier kommt dein Inhalt zur Wirkung, Mangel, Quellen usw.</p>
+    </div>
+  );
+}
+
+export default App;
